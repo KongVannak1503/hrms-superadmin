9e5097c11658a9b7b1ea7384f9899e653870e560
KongVannak1503 <kongvannak1503@gmail.com>
2026-05-12 23:46:20 +0700

    remove localhost

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/super-admin',
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 15000,
     headers: {
         'Accept': 'application/json',
